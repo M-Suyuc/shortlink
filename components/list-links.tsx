a9ee56a2725fd6a4f95f2350fa96fd2fe376f99e
marlon <marlonsuyuc@gmail.com>
2024-07-02 18:42:20 -0600

    add hook use-origin

--- components/list-links.tsx
+++ components/list-links.tsx
@@ -11,6 +11,9 @@ import { useModalContext } from '@/context/modal'
 import { useState } from 'react'
 import axios from 'axios'
 import toast from 'react-hot-toast'
+import { useOrigin } from '@/hooks/use-origin'
+import Modal from './modal-link'
+import { Button } from './ui/button'
 
 interface Props {
   data: Link[]
@@ -18,10 +21,11 @@ interface Props {
 
 export const ListLinks: React.FC<Props> = ({ data }) => {
   const [loading, setloading] = useState(false)
+  const { showModal } = useModalContext()
 
   const router = useRouter()
   const pathname = usePathname()
-  const { showModal } = useModalContext()
+  const origin = useOrigin()
 
   const handleSetting = (id: string) => {
     router.push(`${pathname}/link/${id}`)
@@ -40,11 +44,8 @@ export const ListLinks: React.FC<Props> = ({ data }) => {
     }
   }
 
-  const clipBoard = (shortLink: string) => {}
-
   const onCopy = async (shortLink: string) => {
-    clipBoard(shortLink)
-    const url = `http://localhost:3000/${shortLink}`
+    const url = `${origin}/${shortLink}`
     navigator.clipboard.writeText(url)
     toast.success('Copied')
   }
@@ -52,73 +53,88 @@ export const ListLinks: React.FC<Props> = ({ data }) => {
   return (
     <>
       {data && (
-        <div className='grid gap-4 grid-cols-auto-fill-100  w-full h-full'>
-          {data.map((item: Link) => {
-            const [formatDate] = item.createdAt.toISOString().split('T')
-            return (
-              <article
-                key={item.id}
-                className='flex flex-col gap-2 border border-neutral-700/70 p-4 rounded-md dark:bg-black'
+        <>
+          <Modal title='' isOpen={false} onClose={() => {}}>
+            <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+              <Button
+                disabled={loading}
+                variant='outline'
+                onClick={() => router.back()}
               >
-                <div className='flex justify-between'>
-                  <div>
-                    <p>{item.shortLink}</p>
-                    <h3>{item.url}</h3>
-                  </div>
-                  <div className='flex gap-2'>
-                    <TooltipProvider delayDuration={300}>
-                      <Tooltip>
-                        <TooltipTrigger
-                          asChild
-                          onClick={() => handleSetting(item.id)}
-                        >
-                          <Settings
-                            size={38}
-                            className='cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-800   p-2.5 rounded-md'
-                          />
-                        </TooltipTrigger>
-                        <TooltipContent>Setting</TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
+                Cancel
+              </Button>
+              <Button disabled={loading}>Delete</Button>
+            </div>
+          </Modal>
 
-                    <TooltipProvider delayDuration={300}>
-                      <Tooltip>
-                        <TooltipTrigger
-                          asChild
-                          onClick={() => onCopy(item.shortLink)}
-                        >
-                          <Copy
-                            size={38}
-                            className='cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-800   p-2.5 rounded-md'
-                          />
-                        </TooltipTrigger>
-                        <TooltipContent>Copy</TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
+          <div className='grid gap-4 grid-cols-auto-fill-100  w-full h-full'>
+            {data.map((item: Link) => {
+              const [formatDate] = item.createdAt.toISOString().split('T')
+              return (
+                <article
+                  key={item.id}
+                  className='flex flex-col gap-2 border border-neutral-700/70 p-4 rounded-md dark:bg-black'
+                >
+                  <div className='flex justify-between'>
+                    <div>
+                      <p>{item.shortLink}</p>
+                      <h3>{item.url}</h3>
+                    </div>
+                    <div className='flex gap-2'>
+                      <TooltipProvider delayDuration={300}>
+                        <Tooltip>
+                          <TooltipTrigger
+                            asChild
+                            onClick={() => handleSetting(item.id)}
+                          >
+                            <Settings
+                              size={38}
+                              className='cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-800   p-2.5 rounded-md'
+                            />
+                          </TooltipTrigger>
+                          <TooltipContent>Setting</TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
 
-                    <TooltipProvider delayDuration={300}>
-                      <Tooltip>
-                        <TooltipTrigger
-                          asChild
-                          onClick={() => onDelete(item.id)}
-                        >
-                          <Trash2
-                            size={38}
-                            className='cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-800   p-2.5 rounded-md'
-                          />
-                        </TooltipTrigger>
-                        <TooltipContent>Delete</TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
+                      <TooltipProvider delayDuration={300}>
+                        <Tooltip>
+                          <TooltipTrigger
+                            asChild
+                            onClick={() => onCopy(item.shortLink)}
+                          >
+                            <Copy
+                              size={38}
+                              className='cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-800   p-2.5 rounded-md'
+                            />
+                          </TooltipTrigger>
+                          <TooltipContent>Copy</TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+
+                      <TooltipProvider delayDuration={300}>
+                        <Tooltip>
+                          <TooltipTrigger
+                            asChild
+                            onClick={() => onDelete(item.id)}
+                          >
+                            <Trash2
+                              size={38}
+                              className='cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-800   p-2.5 rounded-md'
+                            />
+                          </TooltipTrigger>
+                          <TooltipContent>Delete</TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </div>
                   </div>
-                </div>
-                <p className='text-end text-sm text-neutral-400'>
-                  {formatDate}
-                </p>
-              </article>
-            )
-          })}
-        </div>
+                  <p className='text-end text-sm text-neutral-400'>
+                    {formatDate}
+                  </p>
+                </article>
+              )
+            })}
+          </div>
+        </>
       )}
     </>
   )

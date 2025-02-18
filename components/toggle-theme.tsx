95c59ee99e33a5e7e848d1fdcd11398085e27606
Marlon-S <marlonsuyuc@gmail.com>
2025-02-17 19:39:53 -0600

    refactor: standardize code formatting and improve readability in ThemeToggle component

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -1,56 +1,56 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { useTheme } from 'next-themes'
+import * as React from "react";
+import { useTheme } from "next-themes";
 
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
-import { Monitor, MoonIcon, SunIcon } from 'lucide-react'
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Monitor, MoonIcon, SunIcon } from "lucide-react";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='ghost' size='icon'>
-          <SunIcon className='size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <MoonIcon className='absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-          <span className='sr-only'>Theme</span>
+        <Button variant="ghost" size="icon">
+          <SunIcon className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Theme</span>
         </Button>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent align='end'>
-        <span className='inline-block p-2'>Theme</span>
+      <DropdownMenuContent align="end">
+        <span className="inline-block p-2 text-sm">Theme</span>
         <DropdownMenuSeparator />
         <DropdownMenuItem
-          onClick={() => setTheme('light')}
-          className='gap-2 text-base'
+          onClick={() => setTheme("light")}
+          className="gap-3 text-sm"
         >
-          <SunIcon className='size-6' />
+          <SunIcon className="size-4" />
           Light
         </DropdownMenuItem>
         <DropdownMenuItem
-          onClick={() => setTheme('dark')}
-          className='gap-2 text-base'
+          onClick={() => setTheme("dark")}
+          className="gap-3 text-sm"
         >
-          <MoonIcon className='size-6' />
+          <MoonIcon className="size-4" />
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem
-          onClick={() => setTheme('system')}
-          className='gap-2 text-base'
+          onClick={() => setTheme("system")}
+          className="gap-3 text-sm"
         >
-          <Monitor className='size-6' />
+          <Monitor className="size-4" />
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }

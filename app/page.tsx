f4ad28676eaaf6577f96012826fe943a2c635a15
Marlon-S <marlonsuyuc@gmail.com>
2025-02-18 17:54:37 -0600

    refactor: replace dashboard link with HomePage component in main layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import { buttonVariants } from '@/components/ui/button'
-import Link from 'next/link'
+import Component from "@/components/HomePage";
 
 export default function Home() {
   return (
-    <main className='flex justify-center items-center gap-4 min-h-[calc(100vh-5rem)] h-full'>
-      <Link
-        href='/dashboard'
-        className={buttonVariants({
-          variant: 'outline',
-          className: 'block text-xl px-10 py-7'
-        })}
-      >
-        Create a new shortLink
-      </Link>
+    <main>
+      <Component />
     </main>
-  )
+  );
 }

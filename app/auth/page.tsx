cccc5688445535282e7bc0e281f25bd9a99ea1fb
Marlon-S <marlonsuyuc@gmail.com>
2025-02-19 14:08:10 -0600

    refactor: clean up AuthPage component and improve layout

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,65 +1,62 @@
-'use client'
+"use client";
 
-import { GithubLogo, GoogleLogo, Logo } from '@/components/logos'
-import { Button } from '@/components/ui/button'
-import { Loader } from 'lucide-react'
-import { signIn } from 'next-auth/react'
-import { useState } from 'react'
+import { GithubLogo, GoogleLogo } from "@/components/logos";
+import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 const socialProviders = [
   {
-    name: 'Continue with Google',
-    icon: <GoogleLogo className='h-5 w-5' />,
-    provider: 'google'
+    name: "Continue with Google",
+    icon: <GoogleLogo className="h-5 w-5" />,
+    provider: "google",
   },
   {
-    name: 'Continue with GitHub',
-    icon: <GithubLogo className='h-5 w-5' />,
-    provider: 'github'
-  }
-]
+    name: "Continue with GitHub",
+    icon: <GithubLogo className="h-5 w-5" />,
+    provider: "github",
+  },
+];
 
 const AuthPage = () => {
-  const [loading, setLoading] = useState<boolean>(false)
-  const [provider, setProvider] = useState<string | null>()
+  const [loading, setLoading] = useState<boolean>(false);
+  const [provider, setProvider] = useState<string | null>();
 
   const handleSocialLogin = async (provider: string) => {
     try {
-      setLoading(true)
-      setProvider(provider)
-      await signIn(provider)
+      setLoading(true);
+      setProvider(provider);
+      await signIn(provider);
     } catch (error) {
-      console.error('An error occurred while trying to sign in')
+      console.error("An error occurred while trying to sign in");
     }
-  }
+  };
 
   return (
-    <div className='flex justify-center items-center gap-4 min-h-[calc(100vh-5rem)] h-full'>
-      <div className='bg-neutral-900 flex flex-col justify-center items-center gap-3 p-8 rounded-md'>
-        <Logo className='size-10' />
-        <p className='text-xl text-neutral-400 pb-4'>
-          Sign in with your preferred provider
-        </p>
+    <div className="flex justify-center gap-4 min-h-[calc(100vh-4.5rem)] h-full">
+      <div className=" flex flex-col  mt-16 gap-4 p-8 rounded-md  h-fit">
+        <p className="text-[32px] font-bold text-white mb-4">Sign In</p>
         {socialProviders.map((sp) => (
           <Button
             key={sp.provider}
-            variant='outline'
-            className='flex gap-4 border-neutral-700 py-6 w-full'
+            variant="outline"
+            className="flex gap-4 border-neutral-700 py-6 px-8 w-full"
             disabled={loading}
             name={sp.name}
             onClick={() => handleSocialLogin(sp.provider)}
           >
             {provider === sp.provider ? (
-              <Loader className='animate-spin' size={18} />
+              <Loader className="animate-spin" size={18} />
             ) : (
-              <div className=''>{sp.icon}</div>
+              <div>{sp.icon}</div>
             )}
             <span>{sp.name}</span>
           </Button>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthPage
+export default AuthPage;

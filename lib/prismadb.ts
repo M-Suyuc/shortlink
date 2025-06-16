b2d96a47378bb17693d34467fb2df83f6c17e8d4
marlon <marlonsuyuc@gmail.com>
2025-06-16 16:49:29 -0600

    refactor: simplify Prisma client initialization and remove unused code

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,23 +1,5 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
+import { PrismaClient } from "@prisma/client";
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
+const prisma = new PrismaClient();
 
-const prismaClientSingleton = () => {
-  const libsql = createClient({
-    url: `${process.env.TURSO_DATABASE_URL}`,
-    authToken: `${process.env.TURSO_AUTH_TOKEN}`
-  })
-
-  const adapter = new PrismaLibSQL(libsql)
-  return new PrismaClient({ adapter })
-}
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
-
-export default prisma
+export default prisma;

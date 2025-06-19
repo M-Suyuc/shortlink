7ab2b114836462765f8a857fca0a8fe5c2015a9a
marlon <marlonsuyuc@gmail.com>
2025-06-18 19:16:32 -0600

    refactor(Navbar): simplify JSX structure and improve readability

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,41 +9,39 @@ const Navbar = async () => {
   const session = await auth()
 
   return (
-    <>
-      <nav className='px-4 flex items-center h-[4.5rem] border-b bg-background'>
-        <div className='max-w-screen-2xl w-full flex flex-wrap items-center justify-between mx-auto'>
-          <div className='flex items-center gap-2'>
-            <span className='p-2.5 rounded-full bg-black'>
-              <Logo className='size-6 ' />
-            </span>
-            <p className='hidden sm:text-white/80 sm:text-lg sm:font-bold'>
-              {session?.user?.name}
-            </p>
-          </div>
-          <div className='flex gap-4 items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
-            <Link href='https://github.com/M-Suyuc/shortlink' target='_blank'>
-              <GithubLogo className='size-6' />
-            </Link>
-            {session ? (
-              <>
-              <MenuProfile session={session?.user} />
-              </>
-            ) : (
-              <Link
-                href='/dashboard'
-                className={buttonVariants({
-                  variant: 'outline',
-                  className: 'group inline-flex text-lg font-light py-5'
-                })}
-              >
-                <span>Get Started</span>
-                <ArrowRight className='ml-2 h-4 w-4 transform transition-transform group-hover:translate-x-1' />
-              </Link>
-            )}
-          </div>
+    <nav className='flex items-center h-[4.5rem] border-b bg-background'>
+      <div className='max-w-screen-2xl w-full flex px-4 flex-wrap items-center justify-between mx-auto'>
+        <div className='flex items-center gap-2'>
+          <span className='p-2.5 rounded-full bg-black'>
+            <Logo className='size-6 ' />
+          </span>
+          <p className='hidden sm:text-white/80 sm:text-lg sm:font-bold'>
+            {session?.user?.name}
+          </p>
         </div>
-      </nav>
-    </>
+        <div className='flex gap-4 items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
+          <Link href='https://github.com/M-Suyuc/shortlink' target='_blank'>
+            <GithubLogo className='size-6' />
+          </Link>
+          {session ? (
+            <>
+              <MenuProfile session={session?.user} />
+            </>
+          ) : (
+            <Link
+              href='/dashboard'
+              className={buttonVariants({
+                variant: 'outline',
+                className: 'group inline-flex text-lg font-light py-5'
+              })}
+            >
+              <span>Get Started</span>
+              <ArrowRight className='ml-2 h-4 w-4 transform transition-transform group-hover:translate-x-1' />
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
   )
 }
 export default Navbar

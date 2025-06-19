720aa424cc63a6197f801ae24f0017af8c47bd1d
marlon <marlonsuyuc@gmail.com>
2025-06-18 19:14:39 -0600

    refactor(DashboardPage): remove LayoutBorder component and simplify layout structure

--- app/(Protected)/dashboard/page.tsx
+++ app/(Protected)/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { auth } from '@/auth'
 import prisma from '@/lib/prismadb'
 import LinkClient from './_components/link-client'
 import { ListLinks } from '@/components/list-links'
-import LayoutBorder from '@/components/layout'
 import { Suspense } from 'react'
 import Loading from '@/components/ui/loading'
 
@@ -30,21 +29,19 @@ const DashboardPage = async ({
   )
 
   return (
-    <>
-      <LayoutBorder>
-        <LinkClient />
-        <Suspense key={search} fallback={<Loading />}>
-          <ListLinks data={filterShortLinks} />
-        </Suspense>
-        {shorts.length === 0 && (
-          <div className='min-h-[calc(100vh-10rem)] grid place-items-center'>
-            <span className='text-xl selection:not-sr-only text-neutral-900 dark:text-neutral-500'>
-              Start by creating your first shortlink!
-            </span>
-          </div>
-        )}
-      </LayoutBorder>
-    </>
+    <div className='max-w-screen-2xl px-4 mx-auto'>
+      <LinkClient />
+      <Suspense key={search} fallback={<Loading />}>
+        <ListLinks data={filterShortLinks} />
+      </Suspense>
+      {shorts.length === 0 && (
+        <div className='min-h-[calc(100vh-10rem)] grid place-items-center'>
+          <span className='text-xl selection:not-sr-only text-neutral-900 dark:text-neutral-500'>
+            Start by creating your first shortlink!
+          </span>
+        </div>
+      )}
+    </div>
   )
 }
 

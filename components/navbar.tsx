4f454b129ff5b2274a22e68a4f94e5a312e53711
M-Suyuc <marlonsuyuc@gmail.com>
2026-01-29 18:53:54 -0600

    feat: add ModeToggle component for theme switching and update Navbar to include it

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,25 +4,22 @@ import { GithubLogo, Logo } from './logos'
 import { auth } from '@/auth'
 import { ArrowRight } from 'lucide-react'
 import { buttonVariants } from './ui/button'
+import { ModeToggle } from './toggle-theme'
 
 const Navbar = async () => {
   const session = await auth()
 
-  return (
-    <nav className='flex items-center h-[4.5rem] border-b bg-background'>
-      <div className='max-w-screen-2xl w-full flex px-4 flex-wrap items-center justify-between mx-auto'>
-        <div className='flex items-center gap-2'>
-          <span className='text-2xl select-none'>
-            <Logo />
+return (
+    <nav className='flex items-center h-18 border-b border-neutral-200/80 dark:border-neutral-200/10 dark:bg-[#030303]'>
+      <div className='max-w-(--breakpoint-2xl) w-full flex px-4 flex-wrap items-center justify-between mx-auto'>
+          <span className='select-none'>
+            <Logo/>
           </span>
-          <p className='hidden sm:text-white/80 sm:text-lg sm:font-bold'>
-            {session?.user?.name}
-          </p>
-        </div>
-        <div className='flex gap-4 items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
-          <Link href='https://github.com/M-Suyuc/shortlink' target='_blank'>
+        <div className='flex items-center space-x-1 h-10'>
+          <Link href='https://github.com/M-Suyuc/shortlink' target='_blank' className='inline-flex items-center p-2 h-full  dark:hover:bg-neutral-800 hover:bg-neutral-100 rounded-md'>
             <GithubLogo className='size-6' />
           </Link>
+          <ModeToggle />
           {session ? (
             <>
               <MenuProfile session={session?.user} />
@@ -32,7 +29,7 @@ const Navbar = async () => {
               href='/dashboard'
               className={buttonVariants({
                 variant: 'outline',
-                className: 'group inline-flex text-lg font-light py-5'
+                className: 'group inline-flex font-light h-full  dark:hover:bg-neutral-800'
               })}
             >
               <span>Get Started</span>
---
+++ components/toggle-theme.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import * as React from "react"
+import { Monitor, Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu> 
+      <DropdownMenuTrigger asChild className="h-full cursor-pointer">
+        <Button variant="ghost" size="sm">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel className="font-semibold border-b border-neutral-200/80 dark:border-neutral-200/10">Theme</DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => setTheme("light")} className="gap-2">
+        <Sun className="h-4 w-4" /> Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")} className="gap-2">
+          <Moon className="h-4 w-4" /> Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")} className="gap-2">
+          <Monitor className="h-4 w-4" /> System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}

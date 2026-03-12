1044865e5eb584cb8694d5437e6282d32f3de162
M-Suyuc <marlonsuyuc@gmail.com>
2026-03-12 17:07:56 -0600

    feat: enhance Logo component with utility class and currentColor for strokes

--- components/logos/index.tsx
+++ components/logos/index.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils"
 
 interface LogoProps extends React.SVGProps<SVGSVGElement> {
   className?: string
@@ -48,6 +49,7 @@ export const Logo = (props: LogoProps) => {
   return (
     <svg
       {...props}
+      className={cn('stroke-black dark:stroke-white fill-none size-7', props.className)}
       xmlns='http://www.w3.org/2000/svg'
       width={32}
       height={32}
@@ -56,14 +58,14 @@ export const Logo = (props: LogoProps) => {
       strokeLinecap='round'
       strokeLinejoin='round'
     >
-      <path d='M9 15l6 -6' stroke='#fff' />
+      <path d='M9 15l6 -6' stroke='currentColor' />
       <path
         d='M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464'
-        stroke='#fff'
+        stroke='currentColor'
       />
       <path
         d='M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463'
-        stroke='#fff'
+        stroke='currentColor'
       />
     </svg>
   )
